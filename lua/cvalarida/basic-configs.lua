28c96644f47046b8ce879b3079b4f41d2829bcc5
Chris Valarida <cvalarida@gmail.com>
2024-01-29 22:43:33 -0800

    System clipboard + highlight on yank

--- lua/cvalarida/basic-configs.lua
+++ lua/cvalarida/basic-configs.lua
@@ -19,3 +19,16 @@ vim.opt.mouse = "a"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
