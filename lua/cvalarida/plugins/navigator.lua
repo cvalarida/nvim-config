297eab9b555dfa072d9cbe670d8ed134330bb9f7
cvalarida <cvalarida@gmail.com>
2024-01-31 14:15:18 -0800

    Tweak navigator to work more like I want it

--- lua/cvalarida/plugins/navigator.lua
+++ lua/cvalarida/plugins/navigator.lua
@@ -5,10 +5,26 @@ local M = {
     'neovim/nvim-lspconfig',
     "nvim-treesitter/nvim-treesitter",
     "williamboman/mason.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({})
+      end,
+    }
   },
   config = function ()
     require("navigator").setup({
       mason = true,
+      treesitter_analysis = true,
+      treesitter_navigation = true,
+      treesitter_analysis_max_num = 100,
+      treesitter_analysis_condense = true,
+      ts_fold = {
+        enable = true,
+        max_lines_scan_comments = 20,
+        disable_filetypes = {'help', 'guihua', 'text'},
+      },
     })
   end
 }
