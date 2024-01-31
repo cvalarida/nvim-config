549510e1158d810583d013bb4bb6ff683b00d517
Chris Valarida <cvalarida@gmail.com>
2024-01-31 11:25:07 -0800

    Add some diagnostic remaps

--- lua/cvalarida/remap.lua
+++ lua/cvalarida/remap.lua
@@ -15,6 +15,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selection up"})
 
 vim.keymap.set("x", "<leader>p", "\"_dP", {desc = "Paste without selection selection"})
 
+-- May remove these if I use ray-x/navigator.lua
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 wk.register({
 				w = {
 								name = "Window",
