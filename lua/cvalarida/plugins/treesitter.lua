077483cfb5a7bdb604d6b979066fd0f17b5c0575
cvalarida <cvalarida@gmail.com>
2024-02-06 11:35:54 -0800

    Tweak tree-sitter

--- lua/cvalarida/plugins/treesitter.lua
+++ lua/cvalarida/plugins/treesitter.lua
@@ -5,11 +5,12 @@ local M = {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
+        auto_install = true,
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
     end
  }
 
