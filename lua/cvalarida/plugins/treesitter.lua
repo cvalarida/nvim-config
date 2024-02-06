d3cb12f91c349335eb4ca5895995af424f32f41c
cvalarida <cvalarida@gmail.com>
2024-02-06 15:36:34 -0800

    Nevermind on auto installing...
    But also add typescript and tsx to the list of ensure_installed

--- lua/cvalarida/plugins/treesitter.lua
+++ lua/cvalarida/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        auto_install = true,
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
- }
+    configs.setup({
+      -- auto_install = true,
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "tsx", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
 
 return { M }
